d3df754b288d67010011d9e2f7c9ce2a3c976266
AndrewMJordan <48300131+AndrewMJordan@users.noreply.github.com>
2021-04-22 00:23:25 +0000

    Deploy: ce22db5 (v0.1.0)

---
+++ Runtime/Core/Scripts/EventData.cs
@@ -0,0 +1,51 @@
+/*
+ *	Copyright (c) 2021, <AUTHOR>
+ *	All rights reserved.
+ *	
+ *	This source code is licensed under the BSD-style license found in the
+ *	LICENSE file in the root directory of this source tree
+ */
+
+using System;
+using UnityEngine;
+
+namespace Andtech.Dataspace
+{
+
+	public class EventDataEventArgs<TValue>
+	{
+		public TValue NewValue { get; internal set; }
+		public TValue OldValue { get; internal set; }
+
+		public EventDataEventArgs(TValue oldValue, TValue newValue)
+		{
+			OldValue = oldValue;
+			NewValue = newValue;
+		}
+	}
+
+	public abstract class EventData<TValue, TEventArgs> where TEventArgs : EventDataEventArgs<TValue>
+	{
+		public TValue Value
+		{
+			get => value;
+			set
+			{
+				var args = GetEventArgs(Value, value);
+
+				this.value = value;
+				OnValueChanged?.Invoke(this, args);
+			}
+		}
+
+		private TValue value;
+
+		public EventData(TValue value) => Value = value;
+
+		public event EventHandler<TEventArgs> OnValueChanged;
+
+		#region ABSTRACT
+		protected abstract TEventArgs GetEventArgs(TValue oldValue, TValue newValue);
+		#endregion
+	}
+}
---
+++ Runtime/Core/Scripts/TypeData.cs
@@ -0,0 +1,98 @@
+/*
+ *	Copyright (c) 2021, <AUTHOR>
+ *	All rights reserved.
+ *	
+ *	This source code is licensed under the BSD-style license found in the
+ *	LICENSE file in the root directory of this source tree
+ */
+
+using Andtech.Dataspace;
+#if DATASPACE_GRIDLOCK_SUPPORT
+using Andtech.Gridlock;
+#endif
+using UnityEngine;
+
+/*
+Float, float
+Int, int
+String, string
+Vector3Int, Vector3Int
+Rotation, Rotation
+===
+public class $0DataEventArgs : EventDataEventArgs<$1>
+{
+public $0DataEventArgs($1 oldValue, $1 newValue) : base(oldValue, newValue) { }
+}
+public class $0Data : EventData<$1, $0DataEventArgs>
+{
+public $0Data($1 value = default) : base(value) { }
+
+protected override $0DataEventArgs GetEventArgs($1 oldValue, $1 newValue) => new $0DataEventArgs(oldValue, newValue);
+}
+/**/
+
+namespace Boilerplate.Dataspace
+{
+
+	public class FloatDataEventArgs : EventDataEventArgs<float>
+	{
+		public FloatDataEventArgs(float oldValue, float newValue) : base(oldValue, newValue) { }
+	}
+
+	public class FloatData : EventData<float, FloatDataEventArgs>
+	{
+		public FloatData(float value = default) : base(value) { }
+
+		protected override FloatDataEventArgs GetEventArgs(float oldValue, float newValue) => new FloatDataEventArgs(oldValue, newValue);
+	}
+
+	public class IntDataEventArgs : EventDataEventArgs<int>
+	{
+		public IntDataEventArgs(int oldValue, int newValue) : base(oldValue, newValue) { }
+	}
+
+	public class IntData : EventData<int, IntDataEventArgs>
+	{
+		public IntData(int value = default) : base(value) { }
+
+		protected override IntDataEventArgs GetEventArgs(int oldValue, int newValue) => new IntDataEventArgs(oldValue, newValue);
+	}
+
+	public class StringDataEventArgs : EventDataEventArgs<string>
+	{
+		public StringDataEventArgs(string oldValue, string newValue) : base(oldValue, newValue) { }
+	}
+
+	public class StringData : EventData<string, StringDataEventArgs>
+	{
+		public StringData(string value = default) : base(value) { }
+
+		protected override StringDataEventArgs GetEventArgs(string oldValue, string newValue) => new StringDataEventArgs(oldValue, newValue);
+	}
+
+	public class Vector3IntDataEventArgs : EventDataEventArgs<Vector3Int>
+	{
+		public Vector3IntDataEventArgs(Vector3Int oldValue, Vector3Int newValue) : base(oldValue, newValue) { }
+	}
+
+	public class Vector3IntData : EventData<Vector3Int, Vector3IntDataEventArgs>
+	{
+		public Vector3IntData(Vector3Int value = default) : base(value) { }
+
+		protected override Vector3IntDataEventArgs GetEventArgs(Vector3Int oldValue, Vector3Int newValue) => new Vector3IntDataEventArgs(oldValue, newValue);
+	}
+
+#if DATASPACE_GRIDLOCK_SUPPORT
+	public class RotationDataEventArgs : EventDataEventArgs<Rotation>
+	{
+		public RotationDataEventArgs(Rotation oldValue, Rotation newValue) : base(oldValue, newValue) { }
+	}
+
+	public class RotationData : EventData<Rotation, RotationDataEventArgs>
+	{
+		public RotationData(Rotation value = default) : base(value) { }
+
+		protected override RotationDataEventArgs GetEventArgs(Rotation oldValue, Rotation newValue) => new RotationDataEventArgs(oldValue, newValue);
+	}
+#endif
+}
