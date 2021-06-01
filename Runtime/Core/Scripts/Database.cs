cdee85eb126ab9b1846dce9d3c25b6c2fdbf34c2
AndrewMJordan <48300131+AndrewMJordan@users.noreply.github.com>
2021-06-01 01:07:18 +0000

    Deploy: 8699ab4 (v0.2.2)

--- Runtime/Core/Scripts/Database.cs
+++ Runtime/Core/Scripts/Database.cs
@@ -13,104 +13,104 @@ using System.Collections.Generic;
 namespace Andtech.Dataspace
 {
 
-	public class DatabaseEventArgs<TKey, TValue> : EventArgs
-	{
-		public readonly TKey Key;
-		public readonly TValue Data;
+    public class DatabaseEventArgs<TKey, TValue> : EventArgs
+    {
+        public readonly TKey Key;
+        public readonly TValue Data;
 
-		public DatabaseEventArgs(TKey key, TValue data)
-		{
-			Key = key;
-			Data = data;
-		}
-	}
+        public DatabaseEventArgs(TKey key, TValue data)
+        {
+            Key = key;
+            Data = data;
+        }
+    }
 
-	public class Database<TKey, TValue>
-		: IDictionary<TKey, TValue>
-	{
-		private readonly IDictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+    public class Database<TKey, TValue>
+        : IDictionary<TKey, TValue>
+    {
+        private readonly IDictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
 
-		#region INTERFACE
-		public void Add(TKey key, TValue value)
-		{
-			dictionary.Add(key, value);
+        #region INTERFACE
+        public void Add(TKey key, TValue value)
+        {
+            dictionary.Add(key, value);
 
-			Created?.Invoke(new DatabaseEventArgs<TKey, TValue>(key, value));
-		}
+            Created?.Invoke(new DatabaseEventArgs<TKey, TValue>(key, value));
+        }
 
-		public bool Remove(TKey key)
-		{
-			if (dictionary.TryGetValue(key, out var value))
-			{
-				Deleted?.Invoke(new DatabaseEventArgs<TKey, TValue>(key, value));
+        public bool Remove(TKey key)
+        {
+            if (dictionary.TryGetValue(key, out var value))
+            {
+                Deleted?.Invoke(new DatabaseEventArgs<TKey, TValue>(key, value));
 
-				return true;
-			}
+                return true;
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		public bool ContainsKey(TKey key)
-		{
-			return dictionary.ContainsKey(key);
-		}
+        public bool ContainsKey(TKey key)
+        {
+            return dictionary.ContainsKey(key);
+        }
 
-		public bool TryGetValue(TKey key, out TValue value)
-		{
-			return dictionary.TryGetValue(key, out value);
-		}
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return dictionary.TryGetValue(key, out value);
+        }
 
-		public TValue this[TKey key]
-		{
-			get => dictionary[key];
-			set => dictionary[key] = value;
-		}
+        public TValue this[TKey key]
+        {
+            get => dictionary[key];
+            set => dictionary[key] = value;
+        }
 
-		public ICollection<TKey> Keys => dictionary.Keys;
-		public ICollection<TValue> Values => dictionary.Values;
+        public ICollection<TKey> Keys => dictionary.Keys;
+        public ICollection<TValue> Values => dictionary.Values;
 
-		public void Add(KeyValuePair<TKey, TValue> item)
-		{
-			dictionary.Add(item);
-		}
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            dictionary.Add(item);
+        }
 
-		public void Clear()
-		{
-			dictionary.Clear();
-		}
+        public void Clear()
+        {
+            dictionary.Clear();
+        }
 
-		public bool Contains(KeyValuePair<TKey, TValue> item)
-		{
-			return dictionary.Contains(item);
-		}
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return dictionary.Contains(item);
+        }
 
-		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-		{
-			dictionary.CopyTo(array, arrayIndex);
-		}
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            dictionary.CopyTo(array, arrayIndex);
+        }
 
-		public bool Remove(KeyValuePair<TKey, TValue> item)
-		{
-			return dictionary.Remove(item);
-		}
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return dictionary.Remove(item);
+        }
 
-		public int Count => dictionary.Count;
-		public bool IsReadOnly => dictionary.IsReadOnly;
+        public int Count => dictionary.Count;
+        public bool IsReadOnly => dictionary.IsReadOnly;
 
-		IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
-		{
-			return dictionary.GetEnumerator();
-		}
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            return dictionary.GetEnumerator();
+        }
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return dictionary.GetEnumerator();
-		}
-		#endregion
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return dictionary.GetEnumerator();
+        }
+        #endregion
 
-		#region EVENT
-		public event Action<DatabaseEventArgs<TKey, TValue>> Created;
-		public event Action<DatabaseEventArgs<TKey, TValue>> Deleted;
-		#endregion
-	}
+        #region EVENT
+        public event Action<DatabaseEventArgs<TKey, TValue>> Created;
+        public event Action<DatabaseEventArgs<TKey, TValue>> Deleted;
+        #endregion
+    }
 }
--- Samples~/Database Example/DatabaseExample.cs
+++ Samples~/Database Example/DatabaseExample.cs
@@ -12,56 +12,56 @@ using UnityEngine;
 namespace Andtech.Dataspace
 {
 
-	public class Animal
-	{
-		public readonly StringData Name;
-		public readonly Guid Id = Guid.NewGuid();
+    public class Animal
+    {
+        public readonly StringData Name;
+        public readonly Guid Id = Guid.NewGuid();
 
-		public Animal(string name)
-		{
-			Name = new StringData(name);
-		}
+        public Animal(string name)
+        {
+            Name = new StringData(name);
+        }
 
-		public override string ToString() => Name.Value;
-	}
+        public override string ToString() => Name.Value;
+    }
 
-	public class DatabaseExample : MonoBehaviour
-	{
-		private readonly Database<Guid, Animal> animals = new Database<Guid, Animal>();
+    public class DatabaseExample : MonoBehaviour
+    {
+        private readonly Database<Guid, Animal> animals = new Database<Guid, Animal>();
 
-		void Start()
-		{
-			animals.Created += Animals_Created;
-			animals.Deleted += Animals_Deleted;
+        void Start()
+        {
+            animals.Created += Animals_Created;
+            animals.Deleted += Animals_Deleted;
 
-			var animal0 = new Animal("Al");
-			var animal1 = new Animal("Barry");
-			var animal2 = new Animal("Charlie");
+            var animal0 = new Animal("Al");
+            var animal1 = new Animal("Barry");
+            var animal2 = new Animal("Charlie");
 
-			animals.Add(animal0.Id, animal0);
-			animals.Add(animal1.Id, animal1);
-			animals.Add(animal2.Id, animal2);
+            animals.Add(animal0.Id, animal0);
+            animals.Add(animal1.Id, animal1);
+            animals.Add(animal2.Id, animal2);
 
-			animal0.Name.Value = "Phillip";
-		}
+            animal0.Name.Value = "Phillip";
+        }
 
-		private void Animals_Created(DatabaseEventArgs<Guid, Animal> e)
-		{
-			e.Data.Name.OnValueChanged += Name_OnValueChanged;
+        private void Animals_Created(DatabaseEventArgs<Guid, Animal> e)
+        {
+            e.Data.Name.OnValueChanged += Name_OnValueChanged;
 
-			Debug.Log($"{e.Data} was created");
-		}
+            Debug.Log($"{e.Data} was created");
+        }
 
-		private void Name_OnValueChanged(string oldValue, string newValue)
-		{
-			Debug.Log($"{nameof(Animal.Name)} was changed from \"{oldValue}\" to \"{newValue}\"");
-		}
+        private void Name_OnValueChanged(string oldValue, string newValue)
+        {
+            Debug.Log($"{nameof(Animal.Name)} was changed from \"{oldValue}\" to \"{newValue}\"");
+        }
 
-		private void Animals_Deleted(DatabaseEventArgs<Guid, Animal> e)
-		{
-			e.Data.Name.OnValueChanged -= Name_OnValueChanged;
+        private void Animals_Deleted(DatabaseEventArgs<Guid, Animal> e)
+        {
+            e.Data.Name.OnValueChanged -= Name_OnValueChanged;
 
-			Debug.Log($"{e.Data} was deleted");
-		}
-	}
+            Debug.Log($"{e.Data} was deleted");
+        }
+    }
 }
